62bbba73f7f190b01420f9b552e4b2505a34182a
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-01-04 19:39:31 +0530

    created oop in java das

---
+++ src/com/intro/B.java
@@ -0,0 +1,12 @@
+package com.intro;
+
+import static com.packages.A.greeting;
+
+public class B {
+
+    public static void message() {
+
+        System.out.println("for B class in the intro package");
+        greeting();
+    }
+}
---
+++ src/com/packages/A.java
@@ -0,0 +1,14 @@
+package com.packages;
+
+public class A {
+
+    public static void main(String[] agrs){
+
+        System.out.println("fsdfdsfs");
+    }
+
+
+    public static void greeting(){
+        System.out.println("this form A class from packages");
+    }
+}
