6af01308efe11bb5c193f31342edc061a655f148
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-01-05 15:34:05 +0530

    inheritances

---
+++ src/inheritance/properties/BoxWeight.java
@@ -0,0 +1,18 @@
+package inheritance.properties;
+
+public class BoxWeight extends Box {
+
+    double weight;
+
+    BoxWeight(){
+        this.weight = -1;
+    }
+
+        BoxWeight(double h,double w, double l,double weight ){
+//        used to initialise values present in parent class
+        super(h,l,w); // what is this?   call the parent class constructor
+
+        this.weight = weight;
+        }
+
+}
--- src/inheritance/properties/Main.java
+++ src/inheritance/properties/Main.java
@@ -14,9 +14,21 @@ public class Main {
 
         Box box3 = new Box(4.2,1.2,3.4);
         System.out.println(box3.h + " "+ box3.l+" "+ box3.w);
+// //////// passing object to another object in the ( )
 
         Box box4 = new Box(box3);
         System.out.println(box4.h + " "+ box4.l+" "+ box4.w);
+
+//        /////////////////////////////////////////////////////////////////////////////
+        // there are many variables in both parent and child classes
+        // you are given access to variables that are in the ref type i.e. BoxWeight
+        // hence, you should have access to weight variable
+        // this also means, that the ones you are trying to access should be initialised
+        // but here, when the obj itself is of type parent class, how will you call the constructor of child class
+        // this is why error
+
+        BoxWeight box5 = new BoxWeight();
+        System.out.println(box5.weight+ " "+box5.h + " "+ box5.l+" "+ box5.w);
 
     }
 }
