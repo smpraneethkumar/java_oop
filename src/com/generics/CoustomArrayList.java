89b34590bafcc4f9a206f9af5e5b7432a33f89d2
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-01-08 09:39:44 +0530

    creating a coustomArrayList

---
+++ src/com/generics/CoustomArrayList.java
@@ -0,0 +1,14 @@
+package com.generics;
+
+import java.util.ArrayList;
+
+public class CoustomArrayList {
+
+    public static void main (String [] agrs){
+        ArrayList list = new ArrayList();
+
+        list.add("sfsdfsdfsd");
+
+        System.out.println(list.get(0));
+    }
+}
