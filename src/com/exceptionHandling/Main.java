f2dbeb129eb4fe42fb32d14147ae7c5e4283afde
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-01-08 12:55:23 +0530

    exceptionhandling

---
+++ src/com/exceptionHandling/Main.java
@@ -0,0 +1,20 @@
+package com.exceptionHandling;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int a = 5;
+        int b = 0;
+
+        try {
+        int c = a/b;
+
+        }catch (Exception e){
+        System.out.println(e.getMessage());
+
+        }
+        System.out.println("hello world");
+    }
+}
